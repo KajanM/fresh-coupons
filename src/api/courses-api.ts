62ad4b5a49201c11d49e916eb0c26b86b5522738
kajan <gmkajan@gmail.com>
2021-08-23 22:18:21 +0530

    feat: resolve data from api

---
+++ src/api/courses-api.ts
@@ -0,0 +1,19 @@
+import yaml from 'js-yaml'
+import { SyncMeta } from '../models/sync-meta'
+import { Course } from '../models/course'
+
+const API_PREFIX = 'https://bitbucket.org/gmkajan/fresh-coupons-data/raw/a1deedeafb9f90198e392d673873b5672f76d822/'
+
+export async function fetchCourses(timestamp: string): Promise<{ [id: string]: Course }> {
+  const response = await fetch(`${API_PREFIX}udemy-${timestamp}.json`)
+  const data = await response.json()
+
+  return data
+}
+
+export async function fetchSyncMeta(): Promise<SyncMeta> {
+  console.log('fetching sync meta')
+  const data = await fetch(`${API_PREFIX}meta.yml`)
+
+  return  yaml.load(await data.text()) as SyncMeta
+}
--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,7 +1,13 @@
-import { courses } from './assets/mock/udemy-1'
+import { fetchCourses, fetchSyncMeta } from './api/courses-api'
 
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(  async function() {
   console.log('extension installed')
+  const meta = await fetchSyncMeta()
+  const lastSyncTime = new Date(meta.last_synced)
+  console.log('meta', meta)
+  console.log('last', lastSyncTime)
+
+  const courses = await fetchCourses(`06-54-23-08-2021`)
 
   chrome.storage.local.set({ courses }, () => {
     console.log('courses initialized')
