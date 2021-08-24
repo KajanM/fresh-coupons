ac5e04b3ebed266551df3938554a82d9e545ef55
kajan <gmkajan@gmail.com>
2021-08-25 03:49:00 +0530

    refactor: extract `onInstalled` logic to separate function

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,8 +1,14 @@
 import { fetchCoursesAsync, fetchSyncMetaAsync } from './api/courses-api'
 import { StorageKeys } from './models/storage-keys'
 
-chrome.runtime.onInstalled.addListener(  async function() {
+chrome.runtime.onInstalled.addListener(onInstalledAsync)
+
+async function onInstalledAsync() {
   console.log('extension installed')
+  await initializeCoursesFromApiAsync()
+}
+
+async function initializeCoursesFromApiAsync() {
   const meta = await fetchSyncMetaAsync()
   console.log('meta', meta)
   if(meta == null) return
@@ -14,5 +20,5 @@ chrome.runtime.onInstalled.addListener(  async function() {
   chrome.storage.local.set({ [StorageKeys.Courses]: courses }, () => {
     console.log('courses initialized')
   })
-})
+}
 
