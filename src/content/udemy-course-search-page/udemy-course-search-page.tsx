ac073e7a33abe6978b2d49e3e8101ba03ea9eb1c
kajan <kajan.14@cse.mrt.ac.lk>
2021-08-29 19:29:05 +0530

    feat: continuously check DOM for new courses

--- src/content/udemy-course-search-page/udemy-course-search-page.tsx
+++ src/content/udemy-course-search-page/udemy-course-search-page.tsx
@@ -1,14 +1,12 @@
 import {StorageKeys} from "../../models/storage-keys";
-import {waitForElmAsync} from "../../helpers/utils";
 
 const isTest = true
+const couponAvailableBadgeClassName = 'fresh-coupons--coupon-available-badge';
 
-setTimeout(mountCouponBadgeAsync, 2000)
+setInterval(mountCouponBadgeAsync, 3000)
 
 async function mountCouponBadgeAsync() {
-  console.log('called')
   const selector = 'a[href^="/course/"]'
-  await waitForElmAsync(selector)
 
   const courseElements: HTMLLinkElement[] = Array.from(document.querySelectorAll(selector))
   if (!courseElements.length) return;
@@ -20,18 +18,18 @@ async function mountCouponBadgeAsync() {
       const course = courses[courseContainerEle.href]
       if(!course && !isTest) return
 
+      if(courseContainerEle.querySelector(`.${couponAvailableBadgeClassName}`)) return
+
       courseContainerEle.style.position = 'relative'
       const imageContainerEle = courseContainerEle.querySelector('[class^="course-card--image-wrapper"]')
-      if(!imageContainerEle) {
-        console.log('course-card--image-wrapper not available')
-        return
-      }
+      if(!imageContainerEle) return
 
       imageContainerEle.appendChild(getCouponAvailableBadgeElement(isTest ? '100% off' : course.discountPercentage))
     })
 
     function getCouponAvailableBadgeElement(discountPercentage: string) {
       const couponAvailableBadge = document.createElement('div')
+      couponAvailableBadge.classList.add(couponAvailableBadgeClassName)
       couponAvailableBadge.style.backgroundColor = "orange"
       couponAvailableBadge.style.color = "white"
       couponAvailableBadge.style.fontWeight = "bold"
