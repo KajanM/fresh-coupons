9bc287e1af1e4e890b2b91bf97129ff6f8074af1
kajan <kajan.14@cse.mrt.ac.lk>
2021-08-29 14:49:06 +0530

    feat: show coupon available badge in course listing views

---
+++ src/content/udemy-course-search-page/udemy-course-search-page.tsx
@@ -0,0 +1,44 @@
+import {StorageKeys} from "../../models/storage-keys";
+import {waitForElmAsync} from "../../helpers/utils";
+
+setTimeout(mountCouponBadgeAsync, 2000)
+
+async function mountCouponBadgeAsync() {
+  console.log('called')
+  const selector = 'a[href^="/course/"]'
+  await waitForElmAsync(selector)
+
+  const courseElements: HTMLLinkElement[] = Array.from(document.querySelectorAll(selector))
+  if (!courseElements.length) return;
+
+  chrome.storage.local.get([StorageKeys.Courses], result => {
+    const courses = result[StorageKeys.Courses]
+
+    courseElements.forEach(courseContainerEle => {
+      const course = courses[courseContainerEle.href]
+      if(!course) return
+
+      courseContainerEle.style.position = 'relative'
+      const imageContainerEle = courseContainerEle.querySelector('[class^="course-card--image-wrapper"]')
+      if(!imageContainerEle) {
+        console.log('course-card--image-wrapper not available')
+        return
+      }
+
+      imageContainerEle.appendChild(getCouponAvailableBadgeElement(course.discountPercentage))
+    })
+
+    function getCouponAvailableBadgeElement(discountPercentage: string) {
+      const couponAvailableBadge = document.createElement('div')
+      couponAvailableBadge.style.backgroundColor = "orange"
+      couponAvailableBadge.style.color = "white"
+      couponAvailableBadge.style.fontWeight = "bold"
+      couponAvailableBadge.style.position = "absolute"
+      couponAvailableBadge.style.bottom = "0"
+      couponAvailableBadge.style.left = "0"
+      couponAvailableBadge.style.padding = "5px 10px"
+      couponAvailableBadge.innerText = `Coupon available (${discountPercentage})`
+      return couponAvailableBadge;
+    }
+  })
+}
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,4 +1,4 @@
-export function changeInputValue(input: HTMLInputElement, value: string | number){
+export function changeInputValue(input: HTMLInputElement, value: string | number) {
   const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
     window.HTMLInputElement.prototype,
     "value"
@@ -6,6 +6,26 @@ export function changeInputValue(input: HTMLInputElement, value: string | number
 
   nativeInputValueSetter?.call(input, value);
 
-  const inputEvent = new Event("input", { bubbles: true });
+  const inputEvent = new Event("input", {bubbles: true});
   input.dispatchEvent(inputEvent);
+}
+
+export function waitForElmAsync(selector: string) {
+  return new Promise(resolve => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver(mutations => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    });
+  });
 }
