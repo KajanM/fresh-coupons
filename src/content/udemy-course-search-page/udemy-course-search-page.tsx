e98409c7a56bf72657f395ef6eab940a3d534fff
kajan <kajan.14@cse.mrt.ac.lk>
2021-08-29 16:33:45 +0530

    Revert "WIP: use `MutationObserver` to add `coupon-available` badge"
    This reverts commit 48a6b14f

--- src/content/udemy-course-search-page/udemy-course-search-page.tsx
+++ src/content/udemy-course-search-page/udemy-course-search-page.tsx
@@ -1,23 +1,9 @@
 import {StorageKeys} from "../../models/storage-keys";
-import {onElementAddedAsync, waitForElmAsync} from "../../helpers/utils";
+import {waitForElmAsync} from "../../helpers/utils";
 
 const isTest = true
 
-// setTimeout(mountCouponBadgeAsync, 2000)
-
-onElementAddedAsync('a[href^="/course/"]').then(courseContainerElements => {
-  courseContainerElements.forEach((courseContainerEle) => {
-    courseContainerEle.style.position = 'relative'
-    const imageContainerEle = courseContainerEle.querySelector('[class^="course-card--image-wrapper"]')
-    // debugger
-    if(!imageContainerEle) {
-      console.log('course-card--image-wrapper not available')
-      return
-    }
-
-    imageContainerEle.appendChild(getCouponAvailableBadgeElement('100% off'))
-  })
-})
+setTimeout(mountCouponBadgeAsync, 2000)
 
 async function mountCouponBadgeAsync() {
   console.log('called')
@@ -44,19 +30,17 @@ async function mountCouponBadgeAsync() {
       imageContainerEle.appendChild(getCouponAvailableBadgeElement(isTest ? '100% off' : course.discountPercentage))
     })
 
+    function getCouponAvailableBadgeElement(discountPercentage: string) {
+      const couponAvailableBadge = document.createElement('div')
+      couponAvailableBadge.style.backgroundColor = "orange"
+      couponAvailableBadge.style.color = "white"
+      couponAvailableBadge.style.fontWeight = "bold"
+      couponAvailableBadge.style.position = "absolute"
+      couponAvailableBadge.style.bottom = "0"
+      couponAvailableBadge.style.left = "0"
+      couponAvailableBadge.style.padding = "5px 10px"
+      couponAvailableBadge.innerText = `Coupon available (${discountPercentage})`
+      return couponAvailableBadge;
+    }
   })
-}
-
-function getCouponAvailableBadgeElement(discountPercentage: string) {
-  const couponAvailableBadge = document.createElement('div')
-  couponAvailableBadge.style.backgroundColor = "orange"
-  couponAvailableBadge.style.color = "white"
-  couponAvailableBadge.style.fontWeight = "bold"
-  couponAvailableBadge.style.position = "absolute"
-  couponAvailableBadge.style.bottom = "0"
-  couponAvailableBadge.style.left = "0"
-  couponAvailableBadge.style.padding = "5px 10px"
-  couponAvailableBadge.innerText = `Coupon available (${discountPercentage})`
-  console.log('rendered')
-  return couponAvailableBadge;
 }
