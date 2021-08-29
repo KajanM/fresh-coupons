0a2ba7983fde8acc11085d299e6171c003131556
kajan <kajan.14@cse.mrt.ac.lk>
2021-08-29 07:24:17 +0530

    refactor: remove unused components

--- src/components/notification/NotificationButton.tsx
+++
@@ -1,6 +0,0 @@
-import { Button, ButtonProps } from '@chakra-ui/react'
-import * as React from 'react'
-
-export const NotificationButton = (props: ButtonProps) => (
-  <Button variant="ghost" fontSize="xl" size="md" px="8" minH="10" borderRadius="none" flex="1" {...props} />
-)
