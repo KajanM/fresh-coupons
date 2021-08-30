cc583000f1248745cc533105a2a056efe7caa473
kajan <kajan.14@cse.mrt.ac.lk>
2021-08-30 12:50:36 +0530

    feat: sync courses periodically

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,8 +1,25 @@
-import { fetchCoursesAsync, fetchSyncMetaAsync } from './api/courses-api'
-import { StorageKeys } from './models/storage-keys'
-import { SyncStorage } from './models/sync-storage'
+import {fetchCoursesAsync, fetchSyncMetaAsync} from './api/courses-api'
+import {StorageKeys} from './models/storage-keys'
+import {SyncStorage} from './models/sync-storage'
+import {AlarmKeys} from "./models/alarm-keys";
+import Alarm = chrome.alarms.Alarm;
+
+const syncIntervalInMinutes = 60
 
 chrome.runtime.onInstalled.addListener(onInstalledAsync)
+
+chrome.alarms.create(AlarmKeys.SyncCourses, {
+  delayInMinutes: syncIntervalInMinutes,
+  periodInMinutes: syncIntervalInMinutes
+})
+
+chrome.alarms.onAlarm.addListener(onSyncCoursesAlarmAsync)
+
+async function onSyncCoursesAlarmAsync(alarm: Alarm) {
+  if (alarm.name !== AlarmKeys.SyncCourses) return
+
+  await initializeCoursesFromApiAsync()
+}
 
 async function onInstalledAsync() {
   console.log('extension installed')
